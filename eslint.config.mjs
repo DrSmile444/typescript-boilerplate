9d922f52f9ccdbe525d91fa43a20eaccf925a5c4
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2024-11-04 21:36:23 +0200

    chore: add eslint playwright plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,22 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import playwright from 'eslint-plugin-playwright';
 
 export default [
+  {
+    ignores: ['node_modules', 'playwright-report', 'test-results'],
+  },
   { files: ['**/*.{js,mjs,cjs,ts}'] },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   eslintPluginPrettierRecommended,
+  {
+    ...playwright.configs['flat/recommended'],
+    files: ['tests/**'],
+    rules: {
+      ...playwright.configs['flat/recommended'].rules,
+    },
+  },
 ];
