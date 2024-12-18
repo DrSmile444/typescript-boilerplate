1932f5e470ac111f7eefe740e801e8b23edd9813
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2024-12-18 14:02:44 +0200

    chore(setup): fix playwright eslint file rules
    (cherry picked from commit 2d9b2ecb0e4d35520bcde81cded35eba7efb0966)

--- .eslint/playwright.eslint.mjs
+++ .eslint/playwright.eslint.mjs
@@ -2,7 +2,7 @@ import playwright from 'eslint-plugin-playwright';
 
 export const playwrightEslint = {
   ...playwright.configs['flat/recommended'],
-  files: ['tests/**'],
+  files: ['src/tests/**'],
   rules: {
     ...playwright.configs['flat/recommended'].rules,
   },
