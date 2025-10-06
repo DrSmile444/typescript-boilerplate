f7af953a7c4216b0c0577d14abfb6a62347994d2
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2025-10-06 14:11:42 +0300

    chore(setup): add includeIgnoreFile

--- .eslint/eslint-rules.eslint.mjs
+++ .eslint/eslint-rules.eslint.mjs
@@ -3,6 +3,7 @@ export default [
     files: ['./.eslint/**/*.{js,mjs,cjs,ts,tsx}', './eslint.config.mjs'],
     rules: {
       'import/no-extraneous-dependencies': 'off',
+      'import/no-unresolved': 'off',
     },
   },
 ];
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+import path from 'node:path';
+
+import { includeIgnoreFile } from '@eslint/compat';
 import pluginJs from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
@@ -17,13 +20,24 @@ import securityEslint from './.eslint/security.eslint.mjs';
 import sonarEslint from './.eslint/sonar.eslint.mjs';
 import unicornEslint from './.eslint/unicorn.eslint.mjs';
 
+const gitignorePath = path.resolve('.', '.gitignore');
+
+console.info('Using .gitignore file at:', gitignorePath);
+
 export default [
+  // Ignore node_modules folder in eslint
   {
     ignores: ['node_modules'],
   },
+  // Ignore .gitignore files/folder in eslint
+  includeIgnoreFile(gitignorePath),
+  // Apply this config to js and ts files only
   { files: ['**/*.{js,mjs,cjs,ts}'] },
+  // Define environment globals
   { languageOptions: { globals: globals.node } },
+  // Javascript Config
   pluginJs.configs.recommended,
+  // TypeScript Config
   ...tseslint.configs.recommended,
   ...tseslint.configs.stylistic,
   ...tseslint.configs.strict,
