c9ec080695a6a29a0770e4daa119f62d7e8a1c25
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2025-10-09 16:21:37 +0300

    chore(eslint): allow any folders inside .eslint directory in project structure config

--- .eslint/node/project-structure-config.eslint.mjs
+++ .eslint/node/project-structure-config.eslint.mjs
@@ -20,7 +20,11 @@ export const folderStructureConfig = createFolderStructure({
     // .eslint folder for ESLint configurations
     {
       name: '.eslint',
-      children: [{ name: getFolderConfig({ type: 'eslint' }) }, { name: '{kebab-case}.eslintrc.(json|js|json5)' }],
+      children: [
+        { name: getFolderConfig({ type: 'eslint' }) },
+        { name: '{kebab-case}.eslintrc.(json|js|json5)' },
+        { name: '*', children: [] }, // Allow any folders inside .eslint
+      ],
     },
 
     // Source code folder
