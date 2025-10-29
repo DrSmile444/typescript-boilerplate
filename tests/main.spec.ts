abc72cf44e1f25eaf20243a1334956fe889a2f21
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2025-10-29 09:57:11 +0200

    test: update describe block for showHello function to use string literal

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -9,7 +9,7 @@ import { OVERALL_PROJECT_RELEASE_VERSION } from '../src/version';
  * Unit tests for showHello function.
  * @see src/main.ts
  */
-describe(showHello, () => {
+describe('showHello', () => {
   it('should return "Hello world"', () => {
     expect(showHello()).toBe(`Hello world ${environmentConfig.CI} ${OVERALL_PROJECT_RELEASE_VERSION}`);
   });
