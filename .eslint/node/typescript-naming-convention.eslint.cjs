0a735819b672de2ca94221b48e838d172df312e9
Dmytro Vakulenko <33941060+DrSmile444@users.noreply.github.com>
2025-11-29 10:44:41 +0200

    chore: update version to 1.2 and modify boolean variable naming prefixes

--- .eslint/node/typescript-naming-convention.eslint.cjs
+++ .eslint/node/typescript-naming-convention.eslint.cjs
@@ -1,7 +1,7 @@
 /**
  * @author Dmytro Vakulenko
  * @description Best practices for naming in JS / TS
- * @version 1.1
+ * @version 1.2
  * @legacy This is a legacy config and must be used through Eslint compat.
  * */
 module.exports = {
@@ -109,7 +109,7 @@ module.exports = {
           },
           {
             format: ['PascalCase'],
-            prefix: ['is', 'should', 'has', 'can', 'did', 'will'],
+            prefix: ['is', 'are', 'has', 'can', 'should', 'will', 'did'],
             selector: 'variable',
             types: ['boolean'],
           },
